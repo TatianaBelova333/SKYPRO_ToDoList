b22d1d894c1d1091fabd0c5e8544feda9100dd52
TatianaBelova333 <101121235+TatianaBelova333@users.noreply.github.com>
2022-10-22 12:53:43 +0200

    default auth

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -1,38 +1,99 @@
-from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError, AuthenticationFailed, NotAuthenticated
 
-from djoser.serializers import UserSerializer
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from core.models import User
 
 
-class UserRegistrationSerializer(BaseUserRegistrationSerializer):
+class PasswordField(serializers.CharField):
+
+    def __init__(self, **kwargs):
+        kwargs['style'] = {'input_type': 'password'}
+        kwargs.setdefault('write_only', True)
+        super().__init__(**kwargs)
+        self.validators.append(validate_password)
+
+
+class UserSignUpSerializer(serializers.ModelSerializer):
+    password = PasswordField(required=True)
+    password_repeat = PasswordField(required=True)
 
     class Meta:
         model = User
         fields = [
+            'id',
             'username',
             'first_name',
             'last_name',
+            'email',
             'password',
             'password_repeat',
         ]
 
-    def create(self, validated_data):
+    def validate(self, attrs: dict):
+        if attrs['password'] != attrs['password_repeat']:
+            raise ValidationError('The passwords do not match')
+        return attrs
+
+    def create(self, validated_data: dict):
+        del validated_data['password_repeat']
         user = User.objects.create(**validated_data)
-
         user.set_password(validated_data["password"])
-
         user.save()
         return user
 
 
-class CurrentUserSerializer(UserSerializer):
+class UserLoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = PasswordField(required=True)
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'passwords']
+
+    def create(self, validated_data: dict):
+        user = authenticate(
+            username=validated_data['username'],
+            password=validated_data["password"]
+        )
+        if not user:
+            raise AuthenticationFailed
+        return user
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'id',
+            'username',
             'first_name',
             'last_name',
-            'phone',
-            'image',
+            'email',
         ]
+
+
+class UpdatePasswordSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    old_password = PasswordField(read_only=True)
+    new_passwowrd = PasswordField(read_only=True)
+
+    def validate(self, attrs: dict):
+        user = attrs['user']
+        if not user:
+            raise NotAuthenticated
+        if not user.check_password(attrs['old_password']):
+            raise ValidationError('Old password is incorrect')
+        return attrs
+
+    def create(self, validated_data: dict):
+        raise NotImplementedError
+
+    def update(self, instance: User, validated_data: dict):
+        instance.password = make_password(validated_data['new_password'])
+        instance.save(update_fields=('password',))
+        return instance
+
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from rest_framework import status, permissions
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from core.models import User
+from core.serializers import UserSignUpSerializer, UserLoginSerializer, UserProfileSerializer
+
+
+class SignUpView(CreateAPIView):
+    serializer_class = UserSignUpSerializer
+
+
+class LoginView(CreateAPIView):
+    serializer_class = UserLoginSerializer
+
+    def perform_create(self, serializer):
+        login(request=self.request, user=serializer.save())
+"""
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login(request=request, user=serializer.save())
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+"""
+
+
+class ProfileView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserProfileSerializer
+    queryset = User.objects.all()
+
+    def get_object(self) -> User:
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PasswordUpdateView(UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserProfileSerializer
+
+    def get_object(self) -> User:
+        return self.request.user
+
